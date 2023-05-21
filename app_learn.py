c497ffbd3f71cbb6dc99ce04ee698274f6fa5cf3
sabrinamarano <92549985+sabrinamarano@users.noreply.github.com>
2023-05-21 18:17:55 +0200

    Add files via upload

--- app_learn.py
+++ app_learn.py
@@ -46,23 +46,27 @@ df
 
 input_1=st.text_input("What you want to learn?")
 
-if input_1:
-    model = "gpt-3.5-turbo"
-    question = f"Create a plan study for {input_1} in a table "
 
-    response = openai.ChatCompletion.create(
-        model=model,
-        messages=[{"role": "system", "content": "You are a helpful assistant."},{"role": "user", "content": question}])
+model = "gpt-3.5-turbo"
+question = f"Create a plan study for {input_1} in a table "
 
-    answer = response.choices[0].message.content
-    text = answer
-    table_text = text[text.find('|') :]
+response = openai.ChatCompletion.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": question}
+    ]
+)
+
+answer = response.choices[0].message.content
+text = answer
+table_text = text[text.find('|') :]
 
 # Create a DataFrame from the table text
-    df = pd.read_csv(StringIO(table_text), sep='|')
-    df=df.iloc[:, 1:-1]
-    df=df.iloc[1:-1]
-    df
+df = pd.read_csv(StringIO(table_text), sep='|')
+df=df.iloc[:, 1:-1]
+df=df.iloc[1:-1]
+df
 
 
 
